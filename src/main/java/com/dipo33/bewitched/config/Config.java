7342d2f3a34c054e7412a9efaadda30a587f8456
dipo33 <mato.dipcar@gmail.com>
2026-03-07 22:24:58 +0100

    fix: do not crash if mutation config entry is invalid

--- src/main/java/com/dipo33/bewitched/config/Config.java
+++ src/main/java/com/dipo33/bewitched/config/Config.java
@@ -1,11 +1,12 @@
 package com.dipo33.bewitched.config;
 
+import com.dipo33.bewitched.Bewitched;
 import com.dipo33.bewitched.items.mutandis.MutandisMutation;
 
 import java.io.File;
-
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.Objects;
 import java.util.stream.Collectors;
 import net.minecraftforge.common.config.Configuration;
 
@@ -33,26 +34,46 @@ public class Config {
         );
 
         mutandisAdditionalMutations = Arrays.stream(configuration.getStringList(
-            "mutandisAdditionalMutations", Configuration.CATEGORY_GENERAL, new String[]{
-                "bewitched:glint_weed@0 -> bewitched:glint_weed@0",
-                "minecraft:tallgrass@2 -> minecraft:tallgrass@2"
-            }, "Additional mutations in the Mutandis mutation pool"
-        )).map(Parsers::parseMutation).collect(Collectors.toList());
+                "mutandisAdditionalMutations", Configuration.CATEGORY_GENERAL, new String[]{
+                    "bewitched:glint_weed@0 -> bewitched:glint_weed@0",
+                    "minecraft:tallgrass@2 -> minecraft:tallgrass@2"
+                }, "Additional mutations in the Mutandis mutation pool"
+            )).map(Config::tryParseMutation)
+            .filter(Objects::nonNull)
+            .collect(Collectors.toList());
+
         mutandisAdditionalFlowerPotMutations = Arrays.stream(configuration.getStringList(
-            "mutandisAdditionalFlowerPotMutations", Configuration.CATEGORY_GENERAL, new String[]{},
-            "Additional mutations in the Mutandis flower pot mutation pool"
-        )).map(Parsers::parseMutation).collect(Collectors.toList());
+                "mutandisAdditionalFlowerPotMutations", Configuration.CATEGORY_GENERAL, new String[]{},
+                "Additional mutations in the Mutandis flower pot mutation pool"
+            )).map(Config::tryParseMutation)
+            .filter(Objects::nonNull)
+            .collect(Collectors.toList());
+
         mutandisExtremisAdditionalMutations = Arrays.stream(configuration.getStringList(
-            "mutandisExtremisAdditionalMutations", Configuration.CATEGORY_GENERAL, new String[]{},
-            "Additional mutations in the Mutandis Extremis mutation pool"
-        )).map(Parsers::parseMutation).collect(Collectors.toList());
+                "mutandisExtremisAdditionalMutations", Configuration.CATEGORY_GENERAL, new String[]{},
+                "Additional mutations in the Mutandis Extremis mutation pool"
+            )).map(Config::tryParseMutation)
+            .filter(Objects::nonNull)
+            .collect(Collectors.toList());
+
         mutandisExtremisAdditionalGrassMutations = Arrays.stream(configuration.getStringList(
-            "mutandisExtremisAdditionalGrassMutations", Configuration.CATEGORY_GENERAL, new String[]{},
-            "Additional mutations in the Mutandis Extremis special grass mutation pool"
-        )).map(Parsers::parseMutation).collect(Collectors.toList());
+                "mutandisExtremisAdditionalGrassMutations", Configuration.CATEGORY_GENERAL, new String[]{},
+                "Additional mutations in the Mutandis Extremis special grass mutation pool"
+            )).map(Config::tryParseMutation)
+            .filter(Objects::nonNull)
+            .collect(Collectors.toList());
 
         if (configuration.hasChanged()) {
             configuration.save();
+        }
+    }
+
+    private static MutandisMutation tryParseMutation(String line) {
+        try {
+            return Parsers.parseMutation(line);
+        } catch (Exception e) {
+            Bewitched.LOG.warn("Invalid mutandis mutation config entry '{}': {}", line, e.getMessage());
+            return null;
         }
     }
 }
