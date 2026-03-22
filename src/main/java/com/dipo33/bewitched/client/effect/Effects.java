04e2c52ee826663f6a0875f42419c5ed7b3cb149
dipo33 <mato.dipcar@gmail.com>
2026-03-22 01:21:49 +0100

    refactor: add private constructor to prevent instantiation

--- src/main/java/com/dipo33/bewitched/client/effect/Effects.java
+++ src/main/java/com/dipo33/bewitched/client/effect/Effects.java
@@ -1,6 +1,9 @@
 package com.dipo33.bewitched.client.effect;
 
 public class Effects {
+    private Effects() {
+    }
+
     public static final String MUTANDIS_FX = "mutandis";
     public static final String MUTANDIS_EXTREMIS_FX = "mutandis_extremis";
 }
