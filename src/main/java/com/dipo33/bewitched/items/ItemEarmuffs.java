07b55a62183c1eb348ccfb0a092efe64bc4d7f93
Kristýna Fuchsová <kristynafuchsova@seznam.cz>
2026-03-29 17:33:20 +0200

    feat: earmuffs are no longer colorable nor need more render passes by using custom leather enum

--- src/main/java/com/dipo33/bewitched/items/ItemEarmuffs.java
+++ src/main/java/com/dipo33/bewitched/items/ItemEarmuffs.java
@@ -1,9 +1,25 @@
 package com.dipo33.bewitched.items;
 
+import net.minecraft.init.Items;
 import net.minecraft.item.ItemArmor;
+import net.minecraftforge.common.util.EnumHelper;
 
 public class ItemEarmuffs extends ItemArmor {
+
+    private static final ItemArmor.ArmorMaterial BEWITCHED_LEATHER =
+        EnumHelper.addArmorMaterial(
+            "BEWITCHED_LEATHER",
+            5, // 5 (from here) * 11 (from vanilla factor) = 55
+            new int[]{1, 3, 2, 1},
+            15
+        );
+
+    static {
+        BEWITCHED_LEATHER.customCraftingMaterial = Items.leather;
+    }
+
     public ItemEarmuffs() {
-        super(ArmorMaterial.CLOTH, 0, 0);
+        // material, render passes, armor slot (0 for helmet)
+        super(BEWITCHED_LEATHER, 0, 0);
     }
 }
