5a3bd3a28f52c2868dd2985321e95023abd34d8a
Kristýna Fuchsová <kristynafuchsova@seznam.cz>
2026-03-29 19:01:32 +0200

    feat: add description

--- src/main/java/com/dipo33/bewitched/items/ItemEarmuffs.java
+++ src/main/java/com/dipo33/bewitched/items/ItemEarmuffs.java
@@ -2,12 +2,21 @@ package com.dipo33.bewitched.items;
 
 import com.dipo33.bewitched.init.BewitchedItemArmorMaterials;
 
+import java.util.List;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemArmor;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.StatCollector;
 
 public class ItemEarmuffs extends ItemArmor {
 
     public ItemEarmuffs() {
         // material, render passes, armor slot (0 for helmet)
         super(BewitchedItemArmorMaterials.BEWITCHED_LEATHER, 0, 0);
+    }
+
+    @Override
+    public void addInformation(ItemStack stack, EntityPlayer player, List<String> tooltips, boolean advancedTooltips) {
+        tooltips.add(StatCollector.translateToLocal(this.getUnlocalizedName(stack) + ".desc"));
     }
 }
