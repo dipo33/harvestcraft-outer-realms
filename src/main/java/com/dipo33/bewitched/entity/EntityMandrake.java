ad3b336451d7a78585ce2568e3722be185c47487
dipo33 <mato.dipcar@gmail.com>
2026-04-19 22:10:17 +0200

    feat: mandrake entity screams and applies effect to nearby players

--- src/main/java/com/dipo33/bewitched/entity/EntityMandrake.java
+++ src/main/java/com/dipo33/bewitched/entity/EntityMandrake.java
@@ -2,15 +2,62 @@ package com.dipo33.bewitched.entity;
 
 import com.dipo33.bewitched.init.BewitchedItems;
 
+import java.util.List;
 import net.minecraft.entity.SharedMonsterAttributes;
 import net.minecraft.entity.monster.EntityMob;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.potion.Potion;
+import net.minecraft.potion.PotionEffect;
+import net.minecraft.util.AxisAlignedBB;
 import net.minecraft.world.World;
 
 public class EntityMandrake extends EntityMob {
+
+    private static final double SCREAM_RANGE = 8.0D;
+    private static final double SCREAM_RANGE_SQ = SCREAM_RANGE * SCREAM_RANGE;
+    /** Nausea II for 15 seconds */
+    private static final int NAUSEA_DURATION_TICKS = 15 * 20;
+    private static final int NAUSEA_AMPLIFIER = 1;
+
     public EntityMandrake(final World world) {
         super(world);
         this.setSize(0.6F, 1.2F);
+    }
+
+    @Override
+    public void onLivingUpdate() {
+        super.onLivingUpdate();
+        if (this.worldObj.isRemote || !this.isEntityAlive()) {
+            return;
+        }
+
+        AxisAlignedBB box = this.boundingBox.expand(SCREAM_RANGE, SCREAM_RANGE, SCREAM_RANGE);
+        List<EntityPlayer> players = this.worldObj.getEntitiesWithinAABB(EntityPlayer.class, box);
+
+        for (EntityPlayer player : players) {
+            if (!player.isEntityAlive()) {
+                continue;
+            }
+            if (player.capabilities.isCreativeMode) {
+                continue;
+            }
+            if (player.getDistanceSqToEntity(this) > SCREAM_RANGE_SQ) {
+                continue;
+            }
+            ItemStack helmet = player.inventory.armorInventory[3];
+            if (helmet != null && helmet.getItem() == BewitchedItems.EARMUFFS.get()) {
+                continue;
+            }
+
+            PotionEffect nausea = player.getActivePotionEffect(Potion.confusion);
+            if (nausea != null && nausea.getAmplifier() >= NAUSEA_AMPLIFIER && nausea.getDuration() > 0) {
+                continue;
+            }
+
+            player.addPotionEffect(new PotionEffect(Potion.confusion.id, NAUSEA_DURATION_TICKS, NAUSEA_AMPLIFIER));
+        }
     }
 
     @Override
